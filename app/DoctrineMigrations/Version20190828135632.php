9f09fdfa06b59ae5faeb028ba204d42486595034
vagrant <vagrant@homestead>
2020-02-10 22:28:12 +0000

    Add id

--- app/DoctrineMigrations/Version20190828135632.php
+++ app/DoctrineMigrations/Version20190828135632.php
@@ -12,7 +12,7 @@ class Version20190828135632 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql("CREATE TABLE library (name VARCHAR(255) NOT NULL, description VARCHAR(255) NOT NULL, createdAt DATETIME DEFAULT NULL)");
+        $this->addSql("CREATE TABLE library (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, description VARCHAR(255) NOT NULL, createdAt DATETIME DEFAULT NULL)");
     }
     public function down(Schema $schema):void
     {
